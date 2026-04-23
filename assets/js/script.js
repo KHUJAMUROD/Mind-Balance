25f375c1e5b5b13a9304f698648e88bacffc9af6
HUNTER <xujanov.xujamurod@gmail.com>
2026-04-23 21:22:30 +0500

    M 23.04 21:22

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,31 +2,31 @@
 // Nav Bar //
 
 // 1. Изменение шапки при скролле
-window.addEventListener('scroll', () => {
-    const nav = document.querySelector('.navbar');
-    if (window.scrollY > 50) {
-        nav.classList.add('scrolled');
-    } else {
-        nav.classList.remove('scrolled');
-    }
-});
+// window.addEventListener('scroll', () => {
+//     const nav = document.querySelector('.navbar');
+//     if (window.scrollY > 50) {
+//         nav.classList.add('scrolled');
+//     } else {
+//         nav.classList.remove('scrolled');
+//     }
+// });
 
 // 2. Мобильное меню
-const burger = document.querySelector('.burger');
-const navLinks = document.querySelector('.nav-links');
+// const burger = document.querySelector('.burger');
+// const navLinks = document.querySelector('.nav-links');
 
-burger.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
-    // Анимация бургера
-    burger.classList.toggle('toggle');
-});
+// burger.addEventListener('click', () => {
+//     navLinks.classList.toggle('active');
+//     // Анимация бургера
+//     burger.classList.toggle('toggle');
+// });
 
 // Закрытие меню при клике на ссылку
-document.querySelectorAll('.nav-links a').forEach(link => {
-    link.addEventListener('click', () => {
-        navLinks.classList.remove('active');
-    });
-});
+// document.querySelectorAll('.nav-links a').forEach(link => {
+//     link.addEventListener('click', () => {
+//         navLinks.classList.remove('active');
+//     });
+// });
 
 /* -----------------------------------------------------------------------------------------------------  */
 // Hero //
@@ -175,10 +175,10 @@ const elementsToAnimate = document.querySelectorAll(
 /* -----------------------------------------------------------------------------------------------------  */
 // Nav Bar Burger //
 
-burger.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
-    burger.classList.toggle('toggle'); // Эта строка анимирует полоски в крестик
-});
+// burger.addEventListener('click', () => {
+//     navLinks.classList.toggle('active');
+//     burger.classList.toggle('toggle'); // Эта строка анимирует полоски в крестик
+// });
 
 
 /* -----------------------------------------------------------------------------------------------------  */
